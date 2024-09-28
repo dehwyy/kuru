959a245325d4176e1392b4905ea74e0cc60ac8ee
dehwyy <dehwyy@yandex.ru>
2024-09-29 02:56:50 +0300

    [FEAT] Redirect to `/{serverName}` from '/'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,4 @@
-'use client'
-
-import { AppShell } from '@/app/components/appshell'
-import { Button } from '@mantine/core'
-
+import { redirect } from 'next/navigation'
 export default function Page() {
-  return (
-    <AppShell>
-      <>
-        <Button>Button</Button>
-      </>
-    </AppShell>
-  )
+  redirect('/kuru')
 }
